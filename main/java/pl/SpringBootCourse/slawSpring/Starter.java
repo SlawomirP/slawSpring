e05357db823889484d73fa0cdb9345133119aa9f
slawo <slawekpettke@gmail.com>
2022-08-25 12:51:04 +0200

    2 lesson - wstrzykiwanie przez konstruktor i metode

--- main/java/pl/SpringBootCourse/slawSpring/Starter.java
+++ main/java/pl/SpringBootCourse/slawSpring/Starter.java
@@ -3,6 +3,7 @@ package pl.SpringBootCourse.slawSpring;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.SpringBootCourse.slawSpring.domain.Knight;
+import pl.SpringBootCourse.slawSpring.domain.Quest;
 
 //utworzenie pierwszej klasy z implementacja CommandLineRunner
 //interfejs ze springa z metoda run
@@ -12,10 +13,27 @@ public class Starter implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        // krok 7 - tu ustawiamy nowy obiekt questa
+
+        Quest saveAPrincess = new Quest("Save a princess");
        // stworzenie nowego obiektu knight 1
-        Knight lancelot = new Knight("Lancelot", 29);
+        Knight lancelot = new Knight("Lancelot", 29, saveAPrincess);
 
         System.out.println(lancelot);
+
+        //krok 8
+        Quest killTheDragon = new Quest("Kill the dragon.");
+        //stworzenie kolejnego rycerz 5, on ma miec inny quest
+        //problemem jest ze quest w klasie Knight jest ustawiony "sztywno"
+        Knight percival = new Knight("Percival", 31, killTheDragon);
+        System.out.println(percival);
+
+        //krok 11 - przyklad wstrzykiwania przez metode
+        Knight percival2konstruktor = new Knight("Percival2", 31);
+        System.out.println(percival2konstruktor);
+
+        percival2konstruktor.setQuest(killTheDragon);
+        System.out.println(percival2konstruktor);
 
     }
 }
--- main/java/pl/SpringBootCourse/slawSpring/domain/Knight.java
+++ main/java/pl/SpringBootCourse/slawSpring/domain/Knight.java
@@ -5,13 +5,31 @@ public class Knight {
     private String name;
     private int age;
 //stworzenie pola z obiektem 2
-    private Quest quest;
+    private Quest quest; // z powodu tego pola klasa knight jest zalezna
+                        //od klasy Quest i na odwrot
 
+//krok 6 zamiast ustawiac quest w konstruktorze, robimy tak zaeby byl przekazywany
+    //z nadrzednej klasy, wrzucamy to w parametry
+    public Knight(String name, int age, Quest quest) {
+        this.name = name;
+        this.age = age;
+//        this.quest = new Quest("save a princess"); //4
+        this.quest = quest;
+    }
+
+    //krok 10 - dodajemy nowy konstruktor dodajacy imie i wiek
 
     public Knight(String name, int age) {
         this.name = name;
         this.age = age;
-        this.quest = new Quest("save a princess"); //4
+    }
+
+
+    //krok 9 - drugi typ wstrzykiwania - przez metode (najczesciej seter)
+
+
+    public void setQuest(Quest quest) {
+        this.quest = quest;
     }
 
     @Override
