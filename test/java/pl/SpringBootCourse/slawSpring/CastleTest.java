011c98dd8980d89569acb6cee8b86137ab82512d
slawo <slawekpettke@gmail.com>
2022-08-25 20:28:56 +0200

    Testy pisanie

--- test/java/pl/SpringBootCourse/slawSpring/CastleTest.java
+++ test/java/pl/SpringBootCourse/slawSpring/CastleTest.java
@@ -1,4 +1,18 @@
 package pl.SpringBootCourse.slawSpring;
 
+import org.junit.jupiter.api.Test;
+import pl.SpringBootCourse.slawSpring.domain.Castle;
+import pl.SpringBootCourse.slawSpring.domain.Knight;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CastleTest {
+
+    @Test
+    public void castleToStringMessage(){
+        Knight knight = new Knight();
+        Castle castle = new Castle(knight);
+        String except = "slaw castle <-- knight name: lancelot (29). Quest: save a princess";
+        assertEquals(except, castle.toString());
+    }
 }
