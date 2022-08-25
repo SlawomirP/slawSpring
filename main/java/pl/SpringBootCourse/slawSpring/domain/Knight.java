5bf1941b39ec5c698980f73cb7a70e1d11c073af
slawo <slawekpettke@gmail.com>
2022-08-25 13:41:26 +0200

    BEANS, KONTENER,KONTEKST

--- main/java/pl/SpringBootCourse/slawSpring/domain/Knight.java
+++ main/java/pl/SpringBootCourse/slawSpring/domain/Knight.java
@@ -1,29 +1,21 @@
 package pl.SpringBootCourse.slawSpring.domain;
 
 public class Knight {
-
     private String name;
     private int age;
-    //stworzenie pola z obiektem 2
-    private Quest quest; // z powodu tego pola klasa knight jest zalezna
-    //od klasy Quest i na odwrot
+    private Quest quest;
 
-    //krok 6 zamiast ustawiac quest w konstruktorze, robimy tak zaeby byl przekazywany
-    //z nadrzednej klasy, wrzucamy to w parametry
     public Knight(String name, int age, Quest quest) {
         this.name = name;
         this.age = age;
-//        this.quest = new Quest("save a princess"); //4
         this.quest = quest;
     }
 
-    //krok 10 - dodajemy nowy konstruktor dodajacy imie i wiek
     public Knight(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    //krok 9 - drugi typ wstrzykiwania - przez metode (najczesciej seter)
     public void setQuest(Quest quest) {
         this.quest = quest;
     }
--- main/java/pl/SpringBootCourse/slawSpring/domain/Quest.java
+++ main/java/pl/SpringBootCourse/slawSpring/domain/Quest.java
@@ -1,8 +1,6 @@
 package pl.SpringBootCourse.slawSpring.domain;
 
-//stworzenie nowej klasy 3
 public class Quest {
-
     private String description;
 
     public Quest(String description) {
