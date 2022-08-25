4e607323cae24c7ff2eea9c9b2e0b5bd598134c3
slawo <slawekpettke@gmail.com>
2022-08-25 15:55:12 +0200

    wstrzykiwanie typow prymitywnych

--- main/java/pl/SpringBootCourse/slawSpring/Starter.java
+++ main/java/pl/SpringBootCourse/slawSpring/Starter.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.SpringBootCourse.slawSpring.domain.Castle;
-import pl.SpringBootCourse.slawSpring.domain.Knight;
-import pl.SpringBootCourse.slawSpring.domain.Quest;
+
 
 @Component
 public class Starter implements CommandLineRunner {
@@ -18,20 +17,5 @@ public class Starter implements CommandLineRunner {
 
         System.out.println(castle);
 
-        Quest saveAPrincess = new Quest("Save a princess");
-        Knight lancelot = new Knight("Lancelot", 29, saveAPrincess);
-        System.out.println(lancelot);
-
-
-        Quest killTheDragon = new Quest("Kill the dragon.");
-        Knight percival = new Knight("Percival", 31, killTheDragon);
-        System.out.println(percival);
-
-        //krok 11 - przyklad wstrzykiwania przez metode
-        Knight percival2konstruktor = new Knight("Percival2", 31);
-        System.out.println(percival2konstruktor);
-
-        percival2konstruktor.setQuest(killTheDragon);
-        System.out.println(percival2konstruktor);
     }
 }
--- main/java/pl/SpringBootCourse/slawSpring/domain/Knight.java
+++ main/java/pl/SpringBootCourse/slawSpring/domain/Knight.java
@@ -1,27 +1,30 @@
 package pl.SpringBootCourse.slawSpring.domain;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Knight {
     private String name;
     private int age;
-    private Quest quest;
+//    private Quest quest;
 
-    public Knight(String name, int age, Quest quest) {
-        this.name = name;
-        this.age = age;
-        this.quest = quest;
-    }
+//    public Knight(String name, int age, Quest quest) {
+//        this.name = name;
+//        this.age = age;
+//        this.quest = quest;
+//    }
 
     public Knight(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public void setQuest(Quest quest) {
-        this.quest = quest;
-    }
+//    public void setQuest(Quest quest) {
+//        this.quest = quest;
+//    }
 
     @Override
     public String toString() {
-        return "knight name: " + name + " (" + age + "). Quest: " + quest;
+        return "knight name: " + name + " (" + age + ").";
     }
 }
