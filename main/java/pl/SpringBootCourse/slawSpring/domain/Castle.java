7721e641630826550971889af5434f037805deef
slawo <slawekpettke@gmail.com>
2022-08-26 18:19:04 +0200

    zapoznanie z plikiem xml

--- main/java/pl/SpringBootCourse/slawSpring/domain/Castle.java
+++ main/java/pl/SpringBootCourse/slawSpring/domain/Castle.java
@@ -8,48 +8,40 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@PropertySource("classpath:Castle.properties")
-@Component
+//@PropertySource("classpath:Castle.properties")
+//@Component
 
 public class Castle {
 
-//    @Value("${my.castle.name}") // nazwa pliku z application.properties
-                                // jezeli tego pliku nie byloby w application properties
-                                // ale mozna napisac wart domyslna
-@Value("${my.castle.name:DefaultName Castle}") // TO ZOSTANIE WZIETE POD UWAGE JEZELI ZASTOSUJEMY WSTRZYKIWANIE
-private String name = "East Watch"; // BEZ WSTRZYKIWANIA ZASOSUJE TA NAZWE
 
-    //@Autowired // wstrzykiwanie bezposrednio do pola
+    //@Value("${my.castle.name:DefaultName Castle}")
+    private String name;
+
     Knight knight;
 
-    //wstrzykiwanie w konstruktorze, dochodzi adnotacja nad konstruktorem
-    @Autowired
+
     public Castle(Knight knight) {
         this.knight = knight;
     }
 
-    //TWORZYMY KONSTRUKTOR DO TESTOW BEZ WSTRZYKIWANIA, problem jest tu ze konstruktor jest public
-    //i kazdy sobie moze utworzyc obiekt do testow w normalnej klasie, troche pomoze zastosowanie
-    //folderu domain i tam wrzucamy test castle, modyfikator dostepu na domyslny czyli pozbywamy sie
-    //public, w tym momencie bedzie on widoczny tylko dla klas w ttej samej paczce
     Castle(String name, Knight knight) {
         this.name = name;
         this.knight = knight;
     }
 
-    @PostConstruct
-    public void momentAfterCreateBean(){
+//    @PostConstruct
+    public void momentAfterCreateBean() {
         System.out.println("------moment after------");
     }
 
-    @PreDestroy
-    public void momentBeforeDeleteBean(){
+//    @PreDestroy
+    public void momentBeforeDeleteBean() {
         System.out.println("------moment before------");
     }
 
     @Override
-    public String toString(){
-      return  this.name + " <-- " + knight;
+    public String toString() {
+        return this.name + " <-- " + knight;
     }
 }
 
